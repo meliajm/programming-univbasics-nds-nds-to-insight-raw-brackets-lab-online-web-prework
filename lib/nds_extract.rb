6ddab9af451311159b0eab8b152153c817772e09
melia miller <meliajm@gmail.com>
2019-11-25 17:59:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,6 +20,7 @@ def directors_totals(nds)
       row_index += 1 
   end
   new_hash
+  
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
