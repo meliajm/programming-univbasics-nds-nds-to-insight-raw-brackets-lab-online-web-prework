fa11550ffcff8e69ad369034b9ecf9f43f836d87
melia miller <meliajm@gmail.com>
2019-11-25 17:39:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,14 +1,32 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+# pp directors_database
 
+# here's what to do
+# get array of directors, length should be 9
+# get array of worldwide_gross for each director for all movies
+# zip arrays together to form hash 
+puts directors_database[0][:movies][5][:worldwide_gross]
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
+  
+  directors_array = []
+  worldwide_gross_array = []
+  row_index = 0 
+  while row_index < directors_database.length do
+    new_index = 0 
+    directors_array.push(directors_database[row_index][:name])
+    while new_index < directors_database[row_index].length do 
+      worldwide_gross_array.push(directors_database[row_index][:movies][new_index][:worldwide_gross])
+      new_index += 1
+    end
+    row_index += 1 
+  end
+  
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
@@ -18,5 +36,10 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  
+  result = Hash[directors_array.zip worldwide_gross_array]
+  puts worldwide_gross_array
+  puts result.length
+  puts directors_array.length
+  result
 end
